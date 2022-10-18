5f5f68ec811cf35973ac6b9c2d4f8ae11d707bcf
joele89 <32238188+joele89@users.noreply.github.com>
2022-10-18 17:35:35 +1100

    Fix schema upgrade

--- Startup.cs
+++ Startup.cs
@@ -88,7 +88,7 @@ namespace acme.net
               goto case "1.0.1.0";
             case "1.0.1.0":
               context.Database.ExecuteSqlRaw("BEGIN TRANSACTION;" +
-                                             "ALTER TABLE dbo.Order ADD" +
+                                             "ALTER TABLE dbo.[Order] ADD" +
                                              "  revocationReason int() NULL;" +
                                              "EXEC sys.sp_updateextendedproperty @Name = N'SchemaVersion', @Value = N'1.0.4.0';" +
                                              "COMMIT");
